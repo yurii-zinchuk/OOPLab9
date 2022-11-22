3988ad06a978334faa463fe41f7101f826b786b3
Yurii Zinchuk <yurii.zinchuk@ucu.edu.ua>
2022-11-22 16:09:39 +0200

    users with db working

--- src/main/java/ua/edu/ua/demo/user/AppUser.java
+++ src/main/java/ua/edu/ua/demo/user/AppUser.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.Date;
 
 @Getter
 @Setter
@@ -20,14 +19,14 @@ public class AppUser {
     private Long id;
     private String email;
     private LocalDate dob;
-
     @Transient
-    private Period age;
+    private int age;
 
     public AppUser(String email, LocalDate dob) {
         this.email = email;
         this.dob = dob;
     }
+
 
     public int getAge() {
         return Period.between(dob, LocalDate.now()).getYears();
